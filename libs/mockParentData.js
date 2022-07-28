9eaa8312f1db6f052443d09822ec8d71969bc369
NoahAldhous <naldhous@hotmail.co.uk>
2022-07-28 13:57:26 +0100

    child table populated

--- libs/mockParentData.js
+++ libs/mockParentData.js
@@ -1,24 +1,24 @@
 export const mockParentData = [
 	{
-      firstname:"Noah",
-      lastname:"Aldhous",
-      passcode: 1234,
-      studentid:"1a2b3c",
-      email: "naldhous@hotmail.co.uk"
+            firstname:"Noah",
+            lastname:"Aldhous",
+            passcode: 1234,
+            studentid:"1a2b3c",
+            email: "naldhous@hotmail.co.uk"
 	},
 	{
-      firstname: "Nasra",
-      lastname: "Saleh",
-      passcode: 3421,
-      studentid: "e345f9",
-      email: "something@gmail.com"
+            firstname: "Nasra",
+            lastname: "Saleh",
+            passcode: 3421,
+            studentid: "e345f9",
+            email: "something@gmail.com"
 	},
 	{
-      firstname: "Chris",
-      lastname: "Meah",
-      passcode: 9999,
-      studentid:"S0CC30",
-      email: "soc@org.uk"
+            firstname: "Chris",
+            lastname: "Meah",
+            passcode: 9999,
+            studentid:"S0CC30",
+            email: "soc@org.uk"
 	},
 
 ]
