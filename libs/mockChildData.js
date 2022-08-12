59e229e4c07db49f5b95db992824d71b4ee90d6e
NoahAldhous <naldhous@hotmail.co.uk>
2022-08-12 11:47:09 +0100

    mock data for parent and child updated

--- libs/mockChildData.js
+++ libs/mockChildData.js
@@ -1,23 +1,38 @@
 export const mockChildData = [
-    {
-        
+    {    
         name: "Noah Jr",
         scoreone: 50,
         timecompleted: '13:03:53',
         datecompleted: 'Thu Aug 11 2022'
     },
     {
-      
         name: "Nasra II",
         scoreone: 34,
         timecompleted: '15:13:20',
-        datecompleted: 'Thu Aug 10 2022 '
+        datecompleted: 'Thu Aug 10 2022'
     },
     {
-
         name: "Herbert",
         scoreone: 15,
         timecompleted: '17:30:10',
-        datecompleted: 'Thu Aug 9 2022'
+        datecompleted: 'Wed Aug 9 2022'
     },
+    {
+        name: 'Hecate',
+        scoreone: 66,
+        timecompleted: '16:25:11',
+        datecompleted: 'Wed Aug 9 2022'
+    },
+    {
+        name: 'Anastasia',
+        scoreone: 82,
+        timecompleted: '18:37:21',
+        datecompleted: 'Wed Aug 9 2022'
+    },
+    {
+        name: 'Caleb',
+        scoreone: 75,
+        timecompleted: '15:24:11',
+        datecompleted: 'Wed Aug 9 2022' 
+    }
 ]
--- libs/mockParentData.js
+++ libs/mockParentData.js
@@ -20,5 +20,26 @@ export const mockParentData = [
             childname: "Herbert",
             email: "pirsonomorgolosta@gmail.com"
 	},
+    {
+            firstname: "Mari",
+            lastname: "Lovell",
+            passcode: 1234,
+            childname: "Hecate",
+            email: "nothankyousomuch@fastmail.com"
+    },
+    {
+            firstname: "Bhupinder",
+            lastname: "Singh",
+            passcode: 1234,
+            childname: "Anastasia",
+            email: "udaam13@gmail.com"
+    },
+    {
+            firstname: "Chuks",
+            lastname: "Igwe",
+            passcode: 1234,
+            childname: "Caleb",
+            email: "rhema24+cypress@gmail.com"
+    }
 
 ]
